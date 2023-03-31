3802a1c8c8c7518b4680981664dab09a619cd91a
salimdaoues <daoues22@gmail.com>
2023-03-31 06:46:24 +0100

    update

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://tp-achat-app:8089/SpringMVC/stock';
+  readonly API_URL = 'http://172.20.10.6:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
